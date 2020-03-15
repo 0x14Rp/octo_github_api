d990e8ecc577d8007696cb3a9535293eca8fe01b
Rafael <elgondu@gmail.com>
2020-03-15 17:14:10 -0300

    refactor app componentand routing

--- octo-profile/src/app/app-routing.module.ts
+++ octo-profile/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SearchComponent} from "./components/search/search.component";
+import {DetailComponent} from "./components/detail/detail.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'search', component:SearchComponent},
+  {path:'detail', component:DetailComponent},
+  {path:'**', pathMatch:'full', redirectTo:'search'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- octo-profile/src/app/app.module.ts
+++ octo-profile/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { DetailComponent } from './components/detail/detail.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
